0525e3e2e91b1ebfffc45935d9f240130e6d59da
Mahdi Taghi <mahditaghi@Mahdis-MacBook-Pro.local>
2025-01-13 18:08:44 +0330

    Implement Adaline and Perceptron Algorithms with Decision Boundary Visualization

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ from sklearn.model_selection import train_test_split
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn.linear_model import Perceptron
+from sklearn.metrics import accuracy_score
+import itertools
 
 #import dataset
 iris = load_iris()
@@ -76,3 +79,48 @@ class Adaline:
 
 adaline = Adaline(learning_rate=0.01, epochs=10)
 adaline.fit(X_train, y_train)
+
+
+
+perceptron = Perceptron(max_iter=40, eta0=0.1, random_state=42)
+perceptron.fit(X_train, y_train)
+
+y_pred_train = perceptron.predict(X_train)
+y_pred_test = perceptron.predict(X_test)
+
+print("Perceptron Training Accuracy:", accuracy_score(y_train, y_pred_train))
+print("Perceptron Testing Accuracy:", accuracy_score(y_test, y_pred_test))
+
+def plot_decision_regions(X, y, classifier, title, filename, test_idx=None, resolution=0.02):
+    markers = ('s', 'x', 'o', '^', 'v')
+    colors = ('red', 'blue', 'lightgreen', 'gray', 'cyan')
+    cmap = plt.cm.RdYlBu
+
+    x1_min, x1_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+    x2_min, x2_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+    xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution),
+                           np.arange(x2_min, x2_max, resolution))
+    Z = classifier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
+    Z = Z.reshape(xx1.shape)
+    plt.contourf(xx1, xx2, Z, alpha=0.3, cmap=cmap)
+    plt.xlim(xx1.min(), xx1.max())
+    plt.ylim(xx2.min(), xx2.max())
+
+    for idx, cl in enumerate(np.unique(y)):
+        plt.scatter(x=X[y == cl, 0],
+                    y=X[y == cl, 1],
+                    alpha=0.8,
+                    c=cmap(idx / 10),
+                    marker=markers[idx],
+                    label=cl)
+
+    plt.title(title)
+    plt.xlabel('Feature 1')
+    plt.ylabel('Feature 2')
+    plt.legend(loc='upper left')
+    plt.savefig(filename)
+    plt.show()
+
+plot_decision_regions(X_train[:, :2], y_train, classifier=perceptron, title='Perceptron - Training set', filename='perceptron_training_set.png')
+
+plot_decision_regions(X_test[:, :2], y_test, classifier=perceptron, title='Perceptron - Testing set', filename='perceptron_testing_set.png')
